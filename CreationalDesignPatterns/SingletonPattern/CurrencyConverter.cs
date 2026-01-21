1ffc7291566d867715140d6e1cc0d8a60f93ce32
nour_essam <nouressam1123@gmail.com>
2026-01-21 17:23:17 +0200

    Singleton Design Pattern

---
+++ CreationalDesignPatterns/SingletonPattern/CurrencyConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    internal class CurrencyConverter
+    {
+        private IEnumerable<ExchangeRate> _rates;
+        private CurrencyConverter()
+        {
+            LoadExchangeRates();
+        }
+        private static object _lock = new object();
+        private static CurrencyConverter instance;
+        public static CurrencyConverter Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new CurrencyConverter();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+
+
+
+
+        private void LoadExchangeRates()
+        {
+            Thread.Sleep(4000);
+            _rates = new List<ExchangeRate>()
+            {
+               new ExchangeRate(baseCurrency: "USD" , targetCurrency: "EGP",rate : 47.38m),
+               new ExchangeRate(baseCurrency: "SAR" , targetCurrency: "USD",rate : 0.27m),
+               new ExchangeRate(baseCurrency: "SAR" , targetCurrency: "EGP",rate : 12.63m)
+            };
+        }
+
+
+        public decimal Convert(string baseCurrency, string targetCurrency, decimal amount)
+        {
+            var exchangeRate = _rates.FirstOrDefault(b => b.BaseCurrency == baseCurrency
+            && b.TargetCurrency == targetCurrency);
+
+            return amount * exchangeRate.Rate;
+
+
+        }
+    }
+}
---
+++ CreationalDesignPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.WriteLine("Enter Base Currency: ");
+                var baseCurrency = Console.ReadLine();
+                Console.WriteLine("Enter Target Currency: ");
+                var targetCurrency = Console.ReadLine();
+                Console.WriteLine("Enter Amount: ");
+                var amount = decimal.Parse(Console.ReadLine());
+                var instance = CurrencyConverter.Instance;
+                var exchangedAmount = instance.Convert(baseCurrency, targetCurrency,amount);
+                Console.WriteLine($"{amount} {baseCurrency} = {exchangedAmount} {targetCurrency}");
+                Console.WriteLine("---------------------------------------");
+            }  
+        }
+    }
+}
